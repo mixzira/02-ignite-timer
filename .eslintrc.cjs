104b18aecfd269364b2149193d2a486e1c326204
Mateus Magalhães <mixzira@outlook.com.br>
2023-10-12 14:06:16 -0300

    Added Rocketseat ESLINT settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/next',
+    'next/core-web-vitals'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
